91447ce76f9455cd55222d51fe4a45a10af75aae
SemihOrhan <semih99orhan@gmail.com>
2019-10-17 15:56:02 +0200

    Updated some editor files
    Added EndPointEditor that check whether addColliders is checked and displayed the other collider values based on that.
    Updated WaveConfiguratorEditor so it doesn't show the SpawnerPickerType enum is PerWaveCustom isn't selected.

---
+++ Assets/Editor/EndPointEditor.cs
@@ -0,0 +1,41 @@
+﻿using SemihOrhan.WaveOne.EndPoints;
+using UnityEditor;
+
+namespace SemihOrhan.WaveOne.CustomEditors
+{
+    [CustomEditor(typeof(EndPoint))]
+    public class EndPointEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            SerializedObject so = new SerializedObject(target);
+
+            SerializedProperty endPoints = so.FindProperty("endPoints");
+            SerializedProperty addColliders = so.FindProperty("addColliders");
+            SerializedProperty triggerColliders = so.FindProperty("triggerColliders");
+            SerializedProperty colliderSize = so.FindProperty("colliderSize");
+            SerializedProperty noEndPointEnemies = so.FindProperty("noEndPointEnemies");
+
+            using (new EditorGUI.DisabledScope(true))
+            {
+                EditorGUILayout.ObjectField("Script", MonoScript.FromMonoBehaviour(target as EndPoint), typeof(EndPoint), false);
+            }
+
+            EditorGUILayout.PropertyField(endPoints, true);
+            EditorGUILayout.PropertyField(addColliders, true);
+
+            using (var group = new EditorGUILayout.FadeGroupScope(System.Convert.ToSingle(addColliders.boolValue)))
+            {
+                if (group.visible)
+                {
+                    EditorGUILayout.PropertyField(triggerColliders, true);
+                    EditorGUILayout.PropertyField(colliderSize, true);
+                }
+            }
+
+            EditorGUILayout.PropertyField(noEndPointEnemies, true);
+
+            so.ApplyModifiedProperties();
+        }
+    } 
+}
--- Assets/Editor/WaveConfiguratorEditor.cs
+++ Assets/Editor/WaveConfiguratorEditor.cs
@@ -1,36 +1,56 @@
-﻿using UnityEngine;
-using UnityEditor;
-using SemihOrhan.WaveOne;
+﻿using UnityEditor;
+using UnityEngine;
+using SemihOrhan.WaveOne.Spawners;
 
-[CustomEditor(typeof(WaveConfigurator))]
-public class WaveConfiguratorEditor : Editor
+namespace SemihOrhan.WaveOne.CustomEditors
 {
-    public override void OnInspectorGUI()
+    [CustomEditor(typeof(WaveConfigurator))]
+    public class WaveConfiguratorEditor : Editor
     {
-        base.OnInspectorGUI();
-
-        WaveConfigurator waveConfig = target as WaveConfigurator;
-
-        Color oldColor = GUI.backgroundColor;
-
-        if (GUILayout.Button("Add components (and destroy old)"))
+        public override void OnInspectorGUI()
         {
-            waveConfig.AddStartPointComponents();
-            waveConfig.AddSpawnerComponents();
-            waveConfig.AddEndPointComponents();
-        }
+            WaveConfigurator waveConfig = target as WaveConfigurator;
+            SerializedObject so = new SerializedObject(target);
 
-        GUI.backgroundColor = Color.red;
-        if (GUILayout.Button("Remove all components"))
-        {
-            if (EditorUtility.DisplayDialog("Delete components",
-                                            "You are about to delete all components other than this (WaveConfigurator), are you sure?",
-                                            "Delete",
-                                            "Cancel"))
+            SerializedProperty startPointType = so.FindProperty("startPointType");
+            SerializedProperty startPointPickerType = so.FindProperty("startPointPickerType");
+            SerializedProperty spawnerType = so.FindProperty("spawnerType");
+            SerializedProperty spawnerPickerType = so.FindProperty("spawnerPickerType");
+            SerializedProperty endPointsType = so.FindProperty("endPointsType");
+
+            EditorGUILayout.PropertyField(startPointType, true);
+            EditorGUILayout.PropertyField(startPointPickerType, true);
+
+            EditorGUILayout.PropertyField(spawnerType, true);
+            if (spawnerType.enumValueIndex == (int)SpawnerEnum.SpawnerType.PerWaveCustom)
             {
-                waveConfig.RemoveAllComponents();
+                EditorGUILayout.PropertyField(spawnerPickerType, true); 
             }
+
+            EditorGUILayout.PropertyField(endPointsType, true);
+
+            Color oldColor = GUI.backgroundColor;
+            if (GUILayout.Button("Add components (and destroy old)"))
+            {
+                waveConfig.AddStartPointComponents();
+                waveConfig.AddSpawnerComponents();
+                waveConfig.AddEndPointComponents();
+            }
+
+            GUI.backgroundColor = Color.red;
+            if (GUILayout.Button("Remove all components"))
+            {
+                if (EditorUtility.DisplayDialog("Delete components",
+                                                "You are about to delete all components other than this (WaveConfigurator), are you sure?",
+                                                "Delete",
+                                                "Cancel"))
+                {
+                    waveConfig.RemoveAllComponents();
+                }
+            }
+            GUI.backgroundColor = oldColor;
+
+            so.ApplyModifiedProperties();
         }
-        GUI.backgroundColor = oldColor;
-    }
+    } 
 }
